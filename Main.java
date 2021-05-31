f72b429fb139b355d3cb5addf40605dcd3378a67
Chaxal94 <84540888+Chaxal94@users.noreply.github.com>
2021-05-31 20:19:56 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape aShape = new Shape();
+        Circle aCircle = new Circle();
+        Rectangle aRectangle = new Rectangle();
+        Square aSquare = new Square();
+        System.out.println(aShape);
+        System.out.println(" ");
+        System.out.println(aCircle);
+        System.out.println(" ");
+        System.out.println(aCircle.getPerimeter());
+        System.out.println(" ");
+        System.out.println(aCircle.getArea());
+        System.out.println(" ");
+        System.out.println(aRectangle);
+        System.out.println(" ");
+        System.out.println(aRectangle.getArea());
+        System.out.println(" ");
+        System.out.println(aRectangle.getPerimeter());
+        System.out.println(" ");
+        System.out.println(aSquare.toString());
+
+
+
+
+
+
+    }
+}
